4b075f5e65ced040b5b684dab1c3ffdf9d46b847
NathanCarr10 <nathancarr18@gmail.com>
2025-01-05 23:51:40 +0000

    created index.ejs and modified app.js

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//EJS
+// EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('C:', 'Users', 'DCwa2024', 'Desktop', 'Project', 'Data-Centric-Project-2024'));
+
 
 //Express
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Start server
 const PORT = 3004;
