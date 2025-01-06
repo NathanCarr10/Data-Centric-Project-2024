daa608be4b42d6cd2c618c3a4d9ea925b146d2b7
NathanCarr10 <nathancarr18@gmail.com>
2025-01-06 00:17:14 +0000

    created students and addStudent.ejs files

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express = require('express')
+var pmysql = require('promise-mysql')
+var path = require('path')  // Add this line
+var app = express()
 
-// EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join('C:', 'Users', 'DCwa2024', 'Desktop', 'Project', 'Data-Centric-Project-2024'));
+// Set up EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '.')) // This tells Express to look for views in the current directory
 
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-//Express
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Database connection pool
+var pool
+pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    password: '',  // Your MySQL password
+    database: 'proj2024mysql'
+})
+.then((p) => {
+    pool = p
+})
+.catch((e) => {
+    console.log("pool error:" + e)
+})
 
+// Home page
 app.get('/', (req, res) => {
-    res.render('index');
-});
+    res.render('index')
+})
 
-// Start server
-const PORT = 3004;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Students page
+app.get('/students', (req, res) => {
+    pool.query('SELECT * FROM student ORDER BY sid')
+    .then((students) => {
+        res.render('students', {students: students})
+    })
+    .catch((error) => {
+        res.send(error)
+    })
+})
+
+app.listen(3004, () => {
+    console.log("Server is running on port 3004")
+})
